7d5de4d27fb35c860935df31f07ffd56cdbbddff
Pedro Lucas <pedrogmobile2@gmail.com>
2024-04-25 20:00:01 -0300

    initialcommit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesFiles = FindFiles(storesDirectory);
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        // The file name will contain the full path, so only check the end of it
+        if (extension.Equals(".json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles) {
+    double salesTotal = 0;
+
+    foreach (var file in salesFiles)
+    {
+        string salesJson = File.ReadAllText(file);
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData (double Total);
